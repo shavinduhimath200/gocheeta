889596c68eef80c04a1fe546b57d7b300d7ae55d
shavinduhimath200 <shavinduhimath200@gmail.com>
2022-08-10 00:54:23 +0530

    Home Body Section 1

--- src/Components/HomeComponents/HomeBody.js
+++ src/Components/HomeComponents/HomeBody.js
@@ -4,8 +4,14 @@ import TaxiIMG from "../../Assets/taxi_img.png"
 function HomeBody() {
   return (
     <div className='HomeBody_container'>
-        <img src={TaxiIMG} alt ="" width={500} height={500} />
-        <h1>Welcome to GoCheeta Cab Service</h1>
+    <div className='Section1'>
+    <img src={TaxiIMG} alt ="" width={500} height={500} />
+    </div>
+
+      <div className='Section2'>
+      <h1>Welcome to GoCheeta Cab Service</h1>
+      <p>hi dnjfdsjf fkfhsfsfb kshfjfb dfffsfe sdsgsg<br/>hkdnkf khkfsf hf mf</p> 
+      </div>
     </div>
   )
 }
--- src/Components/HomeComponents/Navbar.js
+++ src/Components/HomeComponents/Navbar.js
@@ -2,9 +2,18 @@ import React from 'react'
 import "./Navbar.css"
 function Navbar() {
   return (
-    <div className='container'>
-        <h2>GoCheeta</h2>
+  <div className='container'>
+    <div>
+    <h2>GoCheeta</h2>
     </div>
+      <div className='containerpart2'>
+        <h3>About</h3>
+        <h3>Contact Us</h3>
+        <h3>Login</h3>
+        <h3>SignUp</h3>
+      </div>
+  </div>
+
   )
 }
 
