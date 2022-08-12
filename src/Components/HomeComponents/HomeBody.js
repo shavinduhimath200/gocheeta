f30fd81ae273244c20e463e9f00cefc7b3995184
shavinduhimath200 <shavinduhimath200@gmail.com>
2022-08-13 00:18:59 +0530

    Home Body 2

--- src/Components/HomeComponents/HomeBody.js
+++ src/Components/HomeComponents/HomeBody.js
@@ -5,12 +5,13 @@ function HomeBody() {
   return (
     <div className='HomeBody_container'>
     <div className='Section1'>
-    <img src={TaxiIMG} alt ="" width={500} height={500} />
+    <img src={TaxiIMG} alt ="" width={600} height={560} />
     </div>
 
       <div className='Section2'>
-      <h1>Welcome to GoCheeta Cab Service</h1>
-      <p>hi dnjfdsjf fkfhsfsfb kshfjfb dfffsfe sdsgsg<br/>hkdnkf khkfsf hf mf</p> 
+      <h1>Welcome to GoCheeta Cab Service</h1> 
+      <h2>Reliable and Secure Way to Reach <br/> Any Point of the City</h2>
+      <h3>We provide affordable and fast way to find a taxi when and<b/> where you need it.</h3> 
       </div>
     </div>
   )
